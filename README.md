64a650d2e20b7cd17b8bb8ed1c3c2c20df1b548d
Sam Lichlyter <slichlyter12@me.com>
2017-11-07 18:16:14 -0800

    added healthy places and refactored

---
+++ README.md
@@ -0,0 +1,16 @@
+# Welcome
+## About
+This website is designed and developed by me. I’m gonna go out on a limb and assume you’re currently looking at my GitHub. If you are, then please take a look around and see how I built this website. Below is a list of technologies I used and ones I might plan to switch to. Welcome to everything me!
+
+## Technologies:
+### Web Technologies Used: 
+* PHP 7.1
+* CSS 3
+
+##### Future Web Technologies:
+* NodeJS
+* ExpressJS
+
+### Platform Tools Used:
+* Coda 2
+
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once("./libs/head.php"); ?>
+<p id="main_title"><a href="index.php">sam lichlyter</a></p>
+<p id="about">Hi, I'm a software developer with expertise in <b>web</b> and <b>iOS development</b></p>
+<p id="links"><a href="./docs/resume.pdf">Resume</a> | <a href="http://github.com/slichlyter12" target="_blank">GitHub</a> | <a href="#projects">Projects</a></p>
+<ul id="projects">
+	<a href="javascript:alert('Healthy Places is not yet available');"><li class="project"><img class="icon" src="img/healthy-places.png"><p class="title">Healthy Places</p><p class="description">Greenspace Surveys</p></li></a>
+
+</ul>
+
+<?php include_once("./libs/foot.php"); ?>
