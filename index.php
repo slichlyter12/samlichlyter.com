49d2062860ab10ae23caa62fd2c649b69edb9deb
Sam Lichlyter <slichlyter12@me.com>
2018-01-30 13:34:15 -0800

    restructured directory

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php include_once("./libs/head.php"); ?>
 <p id="main_title"><a href="index.php">sam lichlyter</a></p>
 <p id="about">Hi, I'm a software developer with expertise in <b>web</b> and <b>iOS development</b></p>
-<p id="links"><a href="./docs/resume.pdf">Resume</a> | <a href="http://github.com/slichlyter12" target="_blank">GitHub</a> | <a href="#projects">Projects</a></p>
+<p id="links"><a href="./docs/resume.pdf">Resume</a> | <a href="http://github.com/slichlyter12" target="_blank">GitHub</a></p>
 <ul id="projects">
-	<a href="javascript:alert('Healthy Places is not yet available');"><li class="project"><img class="icon" src="img/healthy-places.png"><p class="title">Healthy Places</p><p class="description">Greenspace Surveys</p></li></a>
+	<a href="javascript:alert('Healthy Places is not yet available');"><li class="project"><img class="icon" src="img/healthy-spaces.png"><p class="title">Healthy Spaces</p><p class="description">Greenspace Surveys</p></li></a>
 
 </ul>
 
