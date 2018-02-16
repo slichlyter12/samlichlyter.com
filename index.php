d0b84b4f6845c21a4394d4ed352a232386fca971
Sam Lichlyter <slichlyter12@me.com>
2018-02-16 11:40:50 -0800

    updated Healthy Spaces alert

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <p id="about">Hi, I'm a software developer with expertise in <b>web</b> and <b>iOS development</b></p>
 <p id="links"><a href="./docs/resume.pdf">Resume</a> | <a href="http://github.com/slichlyter12" target="_blank">GitHub</a></p>
 <ul id="projects">
-	<a href="javascript:alert('Healthy Places is not yet available');"><li class="project"><img class="icon" src="img/healthy-spaces.png"><p class="title">Healthy Spaces</p><p class="description">Greenspace Surveys</p></li></a>
+	<a href="javascript:alert('Healthy Spaces is currently in beta');"><li class="project"><img class="icon" src="img/healthy-spaces.png"><p class="title">Healthy Spaces</p><p class="description">Greenspace Surveys</p></li></a>
 
 </ul>
 
